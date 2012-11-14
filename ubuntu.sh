24def0f9eb5a1c047bf2221603e1acf65f24e4f3
He Lishi <sunikbear@gmail.com>
2012-11-14 15:19:41 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,7 @@ sudo apt-get install openssh-server
 sudo apt-get install build-essential git-core curl
 
 # zsh
-sudo apt-get instlal zsh
+sudo apt-get install zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
 
